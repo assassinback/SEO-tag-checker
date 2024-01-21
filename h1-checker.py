5dce4bec2ce6d4db025b7898254af53dc1f822bf
Zia-ur-Rehman Khan <ziakhan1198@gmail.com>
2024-01-21 13:19:59 +0500

    Added more conditions

--- h1-checker.py
+++ h1-checker.py
@@ -7,10 +7,6 @@ source_code = requests.get(home_link)
 soup = BeautifulSoup(source_code.content, 'lxml')
 data = []
 links = []
-# test=soup.find_all('h1')
-# print(len(test))
-# for page in test:
-#       print(page.prettify())
 def remove_duplicates(l): # remove duplicates and unURL string
     for item in l:
         match = re.search("(?P<url>https?://[^\s]+)", item)
@@ -30,41 +26,22 @@ for x in range(len(data)):
         data.remove(data[x])
 x=0
 for x in range(len(data)):
-    data[x]=home_link+data[x]
+    if(data[x].find(home_link)>0 or data[x].find("portals")>0):
+        continue
+    else:
+        data[x]=home_link+data[x]
 x=0
 for x in range(len(data)):
     try:
         source_code = requests.get(data[x])
         soup = BeautifulSoup(source_code.content, 'lxml')
         test=soup.find_all('h1')
-        print("There are "+str(len(test))+" h1 Tags on this link, which is/are:\n")
-        for page in test:
-              print(page.prettify())
-              print("\n")
+        if len(test)>0:
+            print("There is/are "+str(len(test))+" h1 Tags on link "+data[x]+", which is/are:\n")
+            for page in test:
+                  print(page.prettify())
+                  print("\n")
+        else:
+            print("There are "+str(len(test))+" h1 Tags on link "+data[x]+"\n")            
     except Exception as e:
         print("invalid link "+data[x]+"\n")
-# flag = True
-# remove_duplicates(data)
-# while flag:
-#     try:
-#         for link in links:
-#             for j in soup.find_all('a', href=True):
-#                 temp = []
-#                 source_code = requests.get(link)
-#                 soup = BeautifulSoup(source_code.content, 'lxml')
-#                 temp.append(str(j.get('href')))
-#                 remove_duplicates(temp)
-
-#                 if len(links) > 162: # set limitation to number of URLs
-#                     break
-#             if len(links) > 162:
-#                 break
-#         if len(links) > 162:
-#             break
-#     except Exception as e:
-#         print(e)
-#         if len(links) > 162:
-#             break
-
-# for url in links:
-#     print(url)
