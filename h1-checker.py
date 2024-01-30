338a3a3b4125b1fc8c01cdf99406a5dc57f51515
Zia-ur-Rehman Khan <ziakhan1198@gmail.com>
2024-01-30 13:57:24 +0500

    Added image and alt tag checking

--- h1-checker.py
+++ h1-checker.py
@@ -13,7 +13,6 @@ def remove_duplicates(l): # remove duplicates and unURL string
         if match is not None:
             links.append((match.group("url")))
 
-
 for link in soup.find_all('a', href=True):
     data.append(str(link.get('href')))
 remove_duplicates(data)
@@ -33,15 +32,39 @@ for x in range(len(data)):
 x=0
 for x in range(len(data)):
     try:
+        if(data[x].find("javascript:void(0)")>=0 or data[x].find(".png")>=0 or data[x].find(".jpg")>=0 or data[x].find("mailto:")>=0 or data[x].find("tel:")>=0 or data[x].find("propertymanagerwebsites")>0): 
+            data.remove(data[x])
+            x=x-1
+    except Exception as e:
+        continue;
+x=0
+data = list(dict.fromkeys(data))
+for x in range(len(data)):
+    try:
         source_code = requests.get(data[x])
         soup = BeautifulSoup(source_code.content, 'lxml')
         test=soup.find_all('h1')
+        images=soup.find_all('img')
+        for script in images:
+            if script.has_attr('data-src'):
+                continue;
+            else:
+                print("Image with following code does not have data-src attribute")
+                print(script)
+        for script in images:
+            if script.has_attr('alt'):
+                continue;
+            else:
+                print("Image with following code does not have alt attribute")
+                print(script)
         if len(test)>0:
-            print("There is/are "+str(len(test))+" h1 Tags on link "+data[x]+", which is/are:\n")
-            for page in test:
-                  print(page.prettify())
-                  print("\n")
+            continue;
+            # print("There is/are "+str(len(test))+" h1 Tags on link "+data[x]+", which is/are:\n")
+            # for page in test:
+            #       print(page.prettify())
+            #       print("\n")
         else:
             print("There are "+str(len(test))+" h1 Tags on link "+data[x]+"\n")            
     except Exception as e:
-        print("invalid link "+data[x]+"\n")
+        continue;
+        # print("invalid link "+data[x]+"\n")
